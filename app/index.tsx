595c7a8078ffd22cbeab49cbd6478c575c580319
MosesAlcantara <mosesalcantara123@gmail.com>
2025-12-19 16:09:17 +0800

    Installed node modules

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const IndexPage = () => {
   return (
-    <View className="flex-1">
+    <SafeAreaView className="flex-1 p-4">
       <Text className="text-red-500">Index Page</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
